6562d07f4e73737fe417a8c023b483451db0eb54
bopo <ibopo@126.com>
2018-02-05 12:27:13 +0800

    add

---
+++ samples/sample_05.py
@@ -0,0 +1,75 @@
+# coding=utf-8
+from mooquant import strategy
+from mooquant.barfeed.csvfeed import GenericBarFeed
+from mooquant.bar import Frequency
+from mooquant.analyzer import returns
+from mooquant.analyzer import sharpe
+from mooquant.utils import stats
+
+
+class MyStrategy(strategy.BacktestingStrategy):
+    def __init__(self, feed):
+        super(MyStrategy, self).__init__(feed, 10000000)
+        self.__position = None
+
+    def onEnterOk(self, position):
+        execInfo = position.getEntryOrder().getExecutionInfo()
+        self.info("BUY at %.2f" % (execInfo.getPrice()))
+
+    def onEnterCanceled(self, position):
+        self.__position = None
+
+    def onExitOk(self, position):
+        execInfo = position.getExitOrder().getExecutionInfo()
+        self.info("SELL at $%.2f" % (execInfo.getPrice()))
+        self.__position = None
+
+    def onExitCanceled(self, position):
+        self.__position.exitMarket()
+
+    def onBars(self, bars):
+        # 1.我们弄一个简单的策略来假装一下
+        day = bars.getDateTime().date().day
+        if day == 5:
+            self.__position = self.enterLong('a', 1, True)
+        elif day == 10:
+            self.__position = self.enterLong('b', 1, True)
+        elif day == 15:
+            self.__position = self.enterLong('c', 1, True)
+        elif day == 20:
+            self.__position = self.enterLong('d', 1, True)
+
+# 2.读取csv文件.
+feed = GenericBarFeed(Frequency.DAY, None, None)
+feed.addBarsFromCSV("a", "a.csv")
+feed.addBarsFromCSV("b", "b.csv")
+feed.addBarsFromCSV("c", "c.csv")
+feed.addBarsFromCSV("d", "d.csv")
+
+strat = MyStrategy(feed)
+
+# 3.加入分析器
+retAnalyzer = returns.Returns()
+strat.attachAnalyzer(retAnalyzer)
+sharpeRatioAnalyzer = sharpe.SharpeRatio()
+strat.attachAnalyzer(sharpeRatioAnalyzer)
+
+# 4.运行策略
+strat.run()
+
+# 5.输出结果
+
+# 最终的投资组合价值
+print("Final portfolio value: $%.2f" % strat.getResult())
+
+# 年化平均收益率
+print("Anual return: %.2f %%" % (retAnalyzer.getCumulativeReturns()[-1] * 100))
+
+# 平均日收益
+print("Average daily return: %.2f %%" % (stats.mean(retAnalyzer.getReturns()) * 100))
+
+# 每日收益标准开发
+print("Std. dev. daily return: %.4f" % (stats.stddev(retAnalyzer.getReturns())))
+
+# 夏普比率
+print("Sharpe ratio: %.2f" % (sharpeRatioAnalyzer.getSharpeRatio(0)))
---
+++ tools/ts2moo.py
@@ -0,0 +1,71 @@
+import tushare as ts
+import pandas as pd
+import click
+import coloredlogs, logging
+
+logger = logging.getLogger(__name__)
+coloredlogs.install(level='DEBUG', logger=logger)
+
+@click.group()
+@click.option('-v', '--verbose', count=True)
+@click.pass_context
+def cli(ctx, verbose):
+	ctx.obj["VERBOSE"] = verbose
+
+
+@cli.command(help='读取股票行情数据.')
+@click.option('-s', '--symbol', default='600848', help='股票代码')
+@click.option('-k', '--ktype', default='D', help='股票代码')
+@click.option('-b', '--begin', default=None, help='下载模块')
+@click.option('-e', '--end', default=None, help='股票代码')
+def fetch(symbol, ktype, begin, end):
+
+	logger.info('starting...')
+	# 得到15分钟数据（股票600848,始于2016-01-01,止于2016-05-24,15分钟数据）
+	df = ts.get_hist_data(symbol, ktype=ktype)
+	# 数据存盘
+	# data.to_csv('15-600848.csv')
+	# 读出数据，DataFrame格式
+	# df = pd.read_csv('15-600848.csv')
+	# 从df中选取数据段，改变段名；新段'Adj Close'使用原有段'close'的数据
+	logger.info('covering...')
+
+	df2 = pd.DataFrame({'Date Time' : df.index, 'Open' : df['open'],
+	                    'High' : df['high'],'Close' : df['close'],
+	                    'Low' : df['low'],'Volume' : df['volume'],
+	                    'Adj Close':df['close']})
+
+	df2 = df2.sort_index(ascending=True)
+	df2 = df2.reset_index(drop=True)
+
+	# 按照Yahoo格式的要求，调整df2各段的顺序
+	df2['Date Time'] = pd.to_datetime(df2['Date Time'], format='%Y-%m-%d %H:%M:%S')
+	
+	dt = df2.pop('Date Time')
+	df2.insert(0, 'Date Time', dt)
+
+	op = df2.pop('Open')
+	df2.insert(1, 'Open', op)
+
+	high = df2.pop('High')
+	df2.insert(2, 'High', high)
+
+	low = df2.pop('Low')
+	df2.insert(3, 'Low', low)
+
+	close = df2.pop('Close')
+	df2.insert(4, 'Close', close)
+
+	volume = df2.pop('Volume')
+	df2.insert(5, 'Volume', volume)
+
+	# 新格式数据存盘，不保存索引编号
+	df2.to_csv("%s.csv" % symbol, index=False, date_format='%Y-%m-%d %H:%M:%S')
+
+	logger.info('save file.')
+
+def main():
+	cli(obj={})
+
+if __name__ == '__main__':
+	main()
